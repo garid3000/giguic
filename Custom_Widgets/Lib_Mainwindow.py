85ce5a7e5efed8a83881f90d6d4292d470c822b0
garid z <garidzorigoo@gmail.com>
2024-06-25 21:40:35 +0900

    Code structure format

--- Custom_Widgets/Lib_Mainwindow.py
+++ Custom_Widgets/Lib_Mainwindow.py
@@ -1,26 +1,23 @@
 #  ---------- Base libraries -------------------------------------------------------------------------------------------
 #import os
 import subprocess as sp
-
 # import logging
 import cv2
 # import numpy as np
-#from numpy._typing import NDArray
+# from numpy._typing import NDArray
 from datetime import datetime
 
 from PySide6.QtWidgets import QMainWindow, QWidget, QMessageBox
-
 # from PySide6.QtGui import QKeySequence, QShortcut, QColor
 # from PySide6.QtCore import QModelIndex, QDir, Qt
 
 from Custom_UIs.UI_Mainwindow import Ui_MainWindow
 
 path_venv_python = "python3"
-#path_venv_python = "/home/pi/.mainvenv/bin/python3"
+# path_venv_python = "/home/pi/.mainvenv/bin/python3" # might needed in the futrue.
+# good for the python library updates
 path_sscan1    = "/home/pi/4Band-Camera/Custom_Libs/sscan1.py"
-#path_check_cam = "/home/pi/SpectroSphere/check_cam.py"
 path_main_from_gui = "/home/pi/4Band-Camera/py_tab2_8bc_gimbal_manual_2.py"
-#path_bno = "/home/pi/SpectroSphere/gry_bno08x.py"
 
 class TheMainWindow(QMainWindow):
     expo_v4l2 = (1, 2, 5, 10, 20, 39, 78, 156, 312, 625, 1250, 2500)
@@ -30,12 +27,40 @@ class TheMainWindow(QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
-        self.init_expo_dial()
+        self.init_expo_dial_spinbox_callbacks()
+        self.init_motor_dial_spinbox_callbacks()
+        self.init_measurment_starting_ui_callbacks()
+
+        self.ui.pb_send_cmd.clicked.connect(self.send_cmd_over_ssh)
+        self.ui.pb_get_cam0.clicked.connect(self.download_img_and_show)
+        self.ui.b_1shot.clicked.connect(self.update_cmd_for_capturing_single_shot_for_exposure_selection)
+
+    def init_expo_dial_spinbox_callbacks(self) -> None:
+        self.ui.d_expo_1.valueChanged.connect(lambda: self.ui.sp_expo_1.setValue(self.expo_v4l2[self.ui.d_expo_1.value()]))
+        self.ui.d_expo_2.valueChanged.connect(lambda: self.ui.sp_expo_2.setValue(self.expo_v4l2[self.ui.d_expo_2.value()]))
+        self.ui.d_expo_3.valueChanged.connect(lambda: self.ui.sp_expo_3.setValue(self.expo_v4l2[self.ui.d_expo_3.value()]))
+        self.ui.d_expo_4.valueChanged.connect(lambda: self.ui.sp_expo_4.setValue(self.expo_v4l2[self.ui.d_expo_4.value()]))
+        self.ui.d_expo_5.valueChanged.connect(lambda: self.ui.sp_expo_5.setValue(self.expo_v4l2[self.ui.d_expo_5.value()]))
+        self.ui.d_expo_6.valueChanged.connect(lambda: self.ui.sp_expo_6.setValue(self.expo_v4l2[self.ui.d_expo_6.value()]))
+        self.ui.d_expo_7.valueChanged.connect(lambda: self.ui.sp_expo_7.setValue(self.expo_v4l2[self.ui.d_expo_7.value()]))
+        self.ui.d_expo_8.valueChanged.connect(lambda: self.ui.sp_expo_8.setValue(self.expo_v4l2[self.ui.d_expo_8.value()]))
+
+        self.ui.sp_expo_1.valueChanged.connect(self.when_any_expo_spinbox_changed)
+        self.ui.sp_expo_2.valueChanged.connect(self.when_any_expo_spinbox_changed)
+        self.ui.sp_expo_3.valueChanged.connect(self.when_any_expo_spinbox_changed)
+        self.ui.sp_expo_4.valueChanged.connect(self.when_any_expo_spinbox_changed)
+        self.ui.sp_expo_5.valueChanged.connect(self.when_any_expo_spinbox_changed)
+        self.ui.sp_expo_6.valueChanged.connect(self.when_any_expo_spinbox_changed)
+        self.ui.sp_expo_7.valueChanged.connect(self.when_any_expo_spinbox_changed)
+        self.ui.sp_expo_8.valueChanged.connect(self.when_any_expo_spinbox_changed)
+
+    def init_motor_dial_spinbox_callbacks(self) -> None:
         self.ui.d_azi.valueChanged.connect(lambda: self.ui.sp_azi.setValue(self.ui.d_azi.value()))
         self.ui.d_elv.valueChanged.connect(lambda: self.ui.sp_elv.setValue(self.ui.d_elv.value()))
-        self.ui.sp_azi.valueChanged.connect(self.when_manual_azi_changed)
-        self.ui.sp_elv.valueChanged.connect(self.when_manual_elv_changed)
+        self.ui.sp_azi.valueChanged.connect(self.when_motor_azi_or_elv_spinbox_value_changed)
+        self.ui.sp_elv.valueChanged.connect(self.when_motor_azi_or_elv_spinbox_value_changed)
 
+    def init_measurment_starting_ui_callbacks(self) -> None:
         self.ui.hs_azi_0.valueChanged.connect(lambda: self.ui.sb_azi_0.setValue(-self.ui.hs_azi_0.value()))
         self.ui.hs_azi_1.valueChanged.connect(lambda: self.ui.sb_azi_1.setValue( self.ui.hs_azi_1.value()))
         self.ui.hs_elv_0.valueChanged.connect(lambda: self.ui.sb_elv_0.setValue(-(self.ui.hs_elv_0.value()//5*5  )))
@@ -47,30 +72,7 @@ class TheMainWindow(QMainWindow):
         self.ui.sb_elv_1.valueChanged.connect(self.when_measure_cmd_change)
         self.ui.qe_tag.textChanged.connect(self.when_measure_cmd_change)
 
-        self.ui.pb_send_cmd.clicked.connect(self.send_cmd)
-        self.ui.pb_get_cam0.clicked.connect(self.download_img_and_show)
-        self.ui.b_1shot.clicked.connect(self.when_capture_shot)
-
-    def init_expo_dial(self) -> None:
-        self.ui.d_expo_1.valueChanged.connect(lambda: self.ui.sp_expo_1.setValue(self.expo_v4l2[self.ui.d_expo_1.value()]))
-        self.ui.d_expo_2.valueChanged.connect(lambda: self.ui.sp_expo_2.setValue(self.expo_v4l2[self.ui.d_expo_2.value()]))
-        self.ui.d_expo_3.valueChanged.connect(lambda: self.ui.sp_expo_3.setValue(self.expo_v4l2[self.ui.d_expo_3.value()]))
-        self.ui.d_expo_4.valueChanged.connect(lambda: self.ui.sp_expo_4.setValue(self.expo_v4l2[self.ui.d_expo_4.value()]))
-        self.ui.d_expo_5.valueChanged.connect(lambda: self.ui.sp_expo_5.setValue(self.expo_v4l2[self.ui.d_expo_5.value()]))
-        self.ui.d_expo_6.valueChanged.connect(lambda: self.ui.sp_expo_6.setValue(self.expo_v4l2[self.ui.d_expo_6.value()]))
-        self.ui.d_expo_7.valueChanged.connect(lambda: self.ui.sp_expo_7.setValue(self.expo_v4l2[self.ui.d_expo_7.value()]))
-        self.ui.d_expo_8.valueChanged.connect(lambda: self.ui.sp_expo_8.setValue(self.expo_v4l2[self.ui.d_expo_8.value()]))
-        pass
-        self.ui.sp_expo_1.valueChanged.connect(self.when_any_expo_change)
-        self.ui.sp_expo_2.valueChanged.connect(self.when_any_expo_change)
-        self.ui.sp_expo_3.valueChanged.connect(self.when_any_expo_change)
-        self.ui.sp_expo_4.valueChanged.connect(self.when_any_expo_change)
-        self.ui.sp_expo_5.valueChanged.connect(self.when_any_expo_change)
-        self.ui.sp_expo_6.valueChanged.connect(self.when_any_expo_change)
-        self.ui.sp_expo_7.valueChanged.connect(self.when_any_expo_change)
-        self.ui.sp_expo_8.valueChanged.connect(self.when_any_expo_change)
-
-    def when_any_expo_change(self) -> None:
+    def when_any_expo_spinbox_changed(self) -> None:
         self.ui.le_expo_minus_str.setText(
             f"{self.expo_v4l2[min(11, max(0, (self.ui.d_expo_1.value() + self.ui.spinBox.value())))]},"
             f"{self.expo_v4l2[min(11, max(0, (self.ui.d_expo_2.value() + self.ui.spinBox.value())))]},"
@@ -81,7 +83,6 @@ class TheMainWindow(QMainWindow):
             f"{self.expo_v4l2[min(11, max(0, (self.ui.d_expo_7.value() + self.ui.spinBox.value())))]},"
             f"{self.expo_v4l2[min(11, max(0, (self.ui.d_expo_8.value() + self.ui.spinBox.value())))]}"
         )
-
 
         self.ui.le_expo_base_str.setText(
             f"{self.ui.sp_expo_1.value()},"
@@ -105,18 +106,9 @@ class TheMainWindow(QMainWindow):
             f"{self.expo_v4l2[min(11, max(0, (self.ui.d_expo_8.value() + self.ui.spinBox_2.value())))]}"
         )
 
+        self.update_cmd_for_capturing_single_shot_for_exposure_selection()
 
-
-    def when_manual_azi_changed(self) -> None:
-        self.ui.le_cmd2send.setText(
-            f"ssh pi@{self.ui.ip_1.value()}.{self.ui.ip_2.value()}.{self.ui.ip_3.value()}.{self.ui.ip_4.value()}"
-            f" {path_venv_python} {path_sscan1} azi={self.ui.sp_azi.value()} elv={self.ui.sp_elv.value()} wait=yes"
-            f" expos=0,0,0,0,0,0,0,0"
-            f" expos_plus=0,0,0,0,0,0,0,0"
-            f" expos_minus=0,0,0,0,0,0,0,0"
-        )
-
-    def when_manual_elv_changed(self) -> None:
+    def when_motor_azi_or_elv_spinbox_value_changed(self) -> None:
         self.ui.le_cmd2send.setText(
             f"ssh pi@{self.ui.ip_1.value()}.{self.ui.ip_2.value()}.{self.ui.ip_3.value()}.{self.ui.ip_4.value()}"
             f" {path_venv_python} {path_sscan1} azi={self.ui.sp_azi.value()} elv={self.ui.sp_elv.value()} wait=yes"
@@ -143,7 +135,7 @@ class TheMainWindow(QMainWindow):
         )
 
 
-    def when_capture_shot(self) -> None:
+    def update_cmd_for_capturing_single_shot_for_exposure_selection(self) -> None:
         self.ui.le_cmd2send.setText(
             f"ssh pi@{self.ui.ip_1.value()}.{self.ui.ip_2.value()}.{self.ui.ip_3.value()}.{self.ui.ip_4.value()}"
             f" {path_venv_python} {path_main_from_gui}"
@@ -152,8 +144,10 @@ class TheMainWindow(QMainWindow):
             f" expos_plus={self.ui.le_expo_plus_str.text()}"
             f" expos_minus={self.ui.le_expo_minus_str.text()}"
         )
+        self.ui.pb_send_cmd.setText("Capture \n single image")
+        # self.ui.pb_send_cmd. TODO: check how to change button color
 
-    def send_cmd(self) -> None:
+    def send_cmd_over_ssh(self) -> None:
         results = sp.run(
             self.ui.le_cmd2send.text().split(),
             capture_output=True,
@@ -164,6 +158,8 @@ class TheMainWindow(QMainWindow):
             results.stdout + "--------------------------\n" + results.stderr
         )
         self.subprocess_status(results)
+        self.ui.pb_send_cmd.setText("Re-Execute")
+        # self.ui.pb_send_cmd. TODO: check how to change button color
 
     def download_img_and_show(self) -> None:
         results = sp.run(
@@ -177,11 +173,9 @@ class TheMainWindow(QMainWindow):
             results.stdout + "--------------------------\n" + results.stderr
         )
         img = cv2.imread("/tmp/test.png")
-        #np.load(f"/tmp/cam{index}.npy")[:, :, 0].T
-
         self.ui.image_view.setImage(
-            img=img,
-            #levels=img.max(),
+            img=img, # levels=img.max(),
+            levels=255,
             axes={"x":1, "y":0, "c":2}
         )
 
@@ -196,5 +190,5 @@ class TheMainWindow(QMainWindow):
 
         button = dlg.exec_()
 
-        if button == QMessageBox.Ok:
+        if button == QMessageBox.StandardButton.Ok:
             print("OK!")
--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 import sys
-from PySide6.QtWidgets import QApplication # QtCore
+from PySide6.QtWidgets import QApplication, QMessageBox # QtCore
 from Custom_Widgets.Lib_Mainwindow import TheMainWindow
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     w = TheMainWindow()
-    # w.setWindowFlags(
-    #    QtCore.Qt.WindowType.WindowContextHelpButtonHint | QtCore.Qt.WindowType.WindowCloseButtonHint
-    # )
     w.show()
     sys.exit(app.exec_())

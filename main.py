d6d0a34a312cd2d3b3a31d516eeecf2ee804a46a
garid z <garidzorigoo@gmail.com>
2024-07-07 19:06:23 +0900

    Example dark theme test

--- main.py
+++ main.py
@@ -1,9 +1,31 @@
 import sys
 from PySide6.QtWidgets import QApplication, QMessageBox # QtCore
 from Custom_Widgets.Lib_Mainwindow import TheMainWindow
+from PySide6.QtGui import QPalette, QColor
+from PySide6.QtCore import Qt
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+    app.setStyle("Fusion")
+
+    # Now use a palette to switch to dark colors:
+    palette = QPalette()
+    palette.setColor(QPalette.Window, QColor(53, 53, 53))
+    palette.setColor(QPalette.WindowText, Qt.white)
+    palette.setColor(QPalette.Base, QColor(25, 25, 25))
+    palette.setColor(QPalette.AlternateBase, QColor(53, 53, 53))
+    palette.setColor(QPalette.ToolTipBase, Qt.black)
+    palette.setColor(QPalette.ToolTipText, Qt.white)
+    palette.setColor(QPalette.Text, Qt.white)
+    palette.setColor(QPalette.Button, QColor(53, 53, 53))
+    palette.setColor(QPalette.ButtonText, Qt.white)
+    palette.setColor(QPalette.BrightText, Qt.red)
+    palette.setColor(QPalette.Link, QColor(42, 130, 218))
+    palette.setColor(QPalette.Highlight, QColor(42, 130, 218))
+    palette.setColor(QPalette.HighlightedText, Qt.black)
+    app.setPalette(palette)
+
+
     w = TheMainWindow()
     w.show()
     sys.exit(app.exec())

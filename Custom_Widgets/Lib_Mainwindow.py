51c0655a264b2d61282c3af8d2092d23d5278495
garid3000 <garidzorigoo@gmail.com>
2024-09-03 14:48:31 +0900

    adding the ground sky logger

--- Custom_Widgets/Lib_Mainwindow.py
+++ Custom_Widgets/Lib_Mainwindow.py
@@ -1,10 +1,11 @@
 #  ---------- Base libraries -------------------------------------------------------------------------------------------
-#import os
+# import os
 import subprocess as sp
 
 # import logging
 import numpy as np
-#from numpy._typing import NDArray
+
+# from numpy._typing import NDArray
 
 from PySide6.QtWidgets import QMainWindow, QWidget, QMessageBox
 
@@ -14,11 +15,12 @@ from PySide6.QtWidgets import QMainWindow, QWidget, QMessageBox
 from Custom_UIs.UI_Mainwindow import Ui_MainWindow
 
 path_venv_python = "/home/pi/.mainvenv/bin/python3"
-path_sscan1    = "/home/pi/SpectroSphere/custom_libs/sscan1.py"
+path_sscan1 = "/home/pi/SpectroSphere/custom_libs/sscan1.py"
 path_check_cam = "/home/pi/SpectroSphere/check_cam.py"
 path_main_from_gui = "/home/pi/SpectroSphere/main_from_gui.py"
 path_main_drone = "/home/pi/SpectroSphere/main_drone.py"
 path_bno = "/home/pi/SpectroSphere/gry_bno08x.py"
+
 
 class TheMainWindow(QMainWindow):
     expo_v4l2 = (1, 2, 5, 10, 20, 39, 78, 156, 312, 625, 1250, 2500)
@@ -39,9 +41,9 @@ class TheMainWindow(QMainWindow):
         self.ui.sp_gain.valueChanged.connect(self.when_manual_gain_changed)
 
         self.ui.hs_azi_0.valueChanged.connect(lambda: self.ui.sb_azi_0.setValue(-self.ui.hs_azi_0.value()))
-        self.ui.hs_azi_1.valueChanged.connect(lambda: self.ui.sb_azi_1.setValue( self.ui.hs_azi_1.value()))
-        self.ui.hs_elv_0.valueChanged.connect(lambda: self.ui.sb_elv_0.setValue(-(self.ui.hs_elv_0.value()//5*5  )))
-        self.ui.hs_elv_1.valueChanged.connect(lambda: self.ui.sb_elv_1.setValue( (self.ui.hs_elv_1.value()//5*5+1)))
+        self.ui.hs_azi_1.valueChanged.connect(lambda: self.ui.sb_azi_1.setValue(self.ui.hs_azi_1.value()))
+        self.ui.hs_elv_0.valueChanged.connect(lambda: self.ui.sb_elv_0.setValue(-(self.ui.hs_elv_0.value() // 5 * 5)))
+        self.ui.hs_elv_1.valueChanged.connect(lambda: self.ui.sb_elv_1.setValue((self.ui.hs_elv_1.value() // 5 * 5 + 1)))
 
         self.ui.sb_azi_0.valueChanged.connect(self.when_measure_cmd_change)
         self.ui.sb_azi_1.valueChanged.connect(self.when_measure_cmd_change)
@@ -52,14 +54,16 @@ class TheMainWindow(QMainWindow):
         self.ui.pb_send_cmd.clicked.connect(self.send_cmd)
         self.ui.pb_capture_shot.clicked.connect(self.when_capture_shot)
 
-
         self.ui.pb_get_cam0.clicked.connect(lambda: self.download_img_and_show(0))
         self.ui.pb_get_cam1.clicked.connect(lambda: self.download_img_and_show(2))
 
         self.ui.pb_bno_check.clicked.connect(self.when_bno_check_pressed)
         self.ui.pb_bno_save.clicked.connect(self.when_bno_save_pressed)
 
-        self.ui.b_tmux_starter.clicked.connect(self.when_tmux_start_button_clicked)
+        self.ui.b_tmux_starter.clicked.connect(  # self.when_tmux_start_button_clicked
+            lambda: self.ui.le_cmd2send.setText(f"ssh pi@{self.ui.ip_1.value()}.{self.ui.ip_2.value()}.{self.ui.ip_3.value()}.{self.ui.ip_4.value()}" f" tmux new -s py -d")
+        )
+
         self.ui.b_tmux_output.clicked.connect(self.when_tmux_get_tmux_output)
         self.ui.drone_starter.clicked.connect(self.when_starting_drone_measurement)
         self.ui.sp_drone_meas_dur.valueChanged.connect(self.when_starting_drone_measurement)
@@ -68,19 +72,35 @@ class TheMainWindow(QMainWindow):
 
         self.ui.b_data_dir.clicked.connect(self.when_gettting_data_dir_info)
 
+        self.ui.b_sky_tmux_starter.clicked.connect(
+            lambda: self.ui.le_cmd2send.setText(f"ssh pi@{self.ui.ip_sky_1.value()}.{self.ui.ip_sky_2.value()}.{self.ui.ip_sky_3.value()}.{self.ui.ip_sky_4.value()}" f" tmux new -s py -d")
+        )
+        self.ui.d_sky_expo.valueChanged.connect(lambda: self.ui.sp_sky_expo.setValue(self.expo_v4l2[self.ui.d_sky_expo.value()]))
+        self.ui.d_sky_gain.valueChanged.connect(lambda: self.ui.sp_sky_gain.setValue(self.ui.d_sky_gain.value()))
+        self.ui.b_sky_measure.clicked.connect(
+            lambda: self.ui.le_cmd2send.setText(
+                f"ssh pi@{self.ui.ip_sky_1.value()}.{self.ui.ip_sky_2.value()}.{self.ui.ip_sky_3.value()}.{self.ui.ip_sky_4.value()}"
+                f" tmux send -t py.0 "
+                " 'python GndLogger/main2.py"
+                "  op_mode=scan"
+                f" time={self.ui.sp_sky_duration.value()}"
+                f" expo_index={self.ui.d_sky_expo.value()}'"
+                f" ENTER"
+            )
+        )
+        self.ui.b_sky_preview.clicked.connect(self.when_sky_preview_button_clicked)
+        self.ui.b_sky_reboot.clicked.connect(
+            lambda: self.ui.le_cmd2send.setText(f"ssh pi@{self.ui.ip_sky_1.value()}.{self.ui.ip_sky_2.value()}.{self.ui.ip_sky_3.value()}.{self.ui.ip_sky_4.value()} reboot")
+        )
+        self.ui.b_sky_data_dir.clicked.connect(self.when_gettting_data_dir_info_SKY)
 
     def when_bno_check_pressed(self) -> None:
-        self.ui.le_cmd2send.setText(
-            f"ssh pi@{self.ui.ip_1.value()}.{self.ui.ip_2.value()}.{self.ui.ip_3.value()}.{self.ui.ip_4.value()}"
-            f" {path_venv_python} {path_bno}"
-        )
+        self.ui.le_cmd2send.setText(f"ssh pi@{self.ui.ip_1.value()}.{self.ui.ip_2.value()}.{self.ui.ip_3.value()}.{self.ui.ip_4.value()}" f" {path_venv_python} {path_bno}")
 
     def when_bno_save_pressed(self) -> None:
         self.ui.le_cmd2send.setText(
-            f"ssh pi@{self.ui.ip_1.value()}.{self.ui.ip_2.value()}.{self.ui.ip_3.value()}.{self.ui.ip_4.value()}"
-            f" {path_venv_python} {path_bno} out={self.ui.le_bno_save_path.text()}"
+            f"ssh pi@{self.ui.ip_1.value()}.{self.ui.ip_2.value()}.{self.ui.ip_3.value()}.{self.ui.ip_4.value()}" f" {path_venv_python} {path_bno} out={self.ui.le_bno_save_path.text()}"
         )
-
 
     def when_manual_azi_changed(self) -> None:
         self.ui.le_cmd2send.setText(
@@ -102,38 +122,24 @@ class TheMainWindow(QMainWindow):
         )
 
     def when_manual_gain_changed(self) -> None:
-        self.ui.le_cmd2send.setText(
-            f"ssh pi@{self.ui.ip_1.value()}.{self.ui.ip_2.value()}.{self.ui.ip_3.value()}.{self.ui.ip_4.value()}"
-            f" v4l2-ctl -d0 -c gain={self.ui.sp_gain.value()}"
-        )
-
-    def when_tmux_start_button_clicked(self) -> None:
-        self.ui.le_cmd2send.setText(
-            f"ssh pi@{self.ui.ip_1.value()}.{self.ui.ip_2.value()}.{self.ui.ip_3.value()}.{self.ui.ip_4.value()}"
-            f" tmux new -s py -d"
-        )
+        self.ui.le_cmd2send.setText(f"ssh pi@{self.ui.ip_1.value()}.{self.ui.ip_2.value()}.{self.ui.ip_3.value()}.{self.ui.ip_4.value()}" f" v4l2-ctl -d0 -c gain={self.ui.sp_gain.value()}")
 
     def when_tmux_get_tmux_output(self) -> None:
-        self.ui.le_cmd2send.setText(
-            f"ssh pi@{self.ui.ip_1.value()}.{self.ui.ip_2.value()}.{self.ui.ip_3.value()}.{self.ui.ip_4.value()}"
-            f" tmux capture -t py:0.0 -p"
-        )
+        self.ui.le_cmd2send.setText(f"ssh pi@{self.ui.ip_1.value()}.{self.ui.ip_2.value()}.{self.ui.ip_3.value()}.{self.ui.ip_4.value()}" f" tmux capture -t py:0.0 -p")
         self.send_cmd()
 
     def when_gettting_camera_v4l2_info(self) -> None:
-        self.ui.le_cmd2send.setText(
-            f"ssh pi@{self.ui.ip_1.value()}.{self.ui.ip_2.value()}.{self.ui.ip_3.value()}.{self.ui.ip_4.value()}"
-            f" v4l2-ctl -d0 --all"
-        )
+        self.ui.le_cmd2send.setText(f"ssh pi@{self.ui.ip_1.value()}.{self.ui.ip_2.value()}.{self.ui.ip_3.value()}.{self.ui.ip_4.value()}" f" v4l2-ctl -d0 --all")
         self.send_cmd()
 
     def when_gettting_data_dir_info(self) -> None:
-        self.ui.le_cmd2send.setText(
-            f"ssh pi@{self.ui.ip_1.value()}.{self.ui.ip_2.value()}.{self.ui.ip_3.value()}.{self.ui.ip_4.value()}"
-            f" tree /home/pi/data* --du -h"
-        )
+        self.ui.le_cmd2send.setText(f"ssh pi@{self.ui.ip_1.value()}.{self.ui.ip_2.value()}.{self.ui.ip_3.value()}.{self.ui.ip_4.value()}" f" tree /home/pi/data* --du -h")
         self.send_cmd()
 
+    def when_gettting_data_dir_info_SKY(self) -> None:
+        #self.ui.le_cmd2send.setText(f"ssh pi@{self.ui.ip_1.value()}.{self.ui.ip_2.value()}.{self.ui.ip_3.value()}.{self.ui.ip_4.value()}" f" tree /home/pi/data* --du -h")
+        self.ui.le_cmd2send.setText(f"ssh pi@{self.ui.ip_sky_1.value()}.{self.ui.ip_sky_2.value()}.{self.ui.ip_sky_3.value()}.{self.ui.ip_sky_4.value()} tree /home/pi/data* --du -h")
+        self.send_cmd()
 
     def when_starting_drone_measurement(self):
         self.ui.le_cmd2send.setText(
@@ -160,10 +166,7 @@ class TheMainWindow(QMainWindow):
         )
 
     def when_capture_shot(self) -> None:
-        self.ui.le_cmd2send.setText(
-            f"ssh pi@{self.ui.ip_1.value()}.{self.ui.ip_2.value()}.{self.ui.ip_3.value()}.{self.ui.ip_4.value()}"
-            f" {path_venv_python} {path_check_cam}"
-        )
+        self.ui.le_cmd2send.setText(f"ssh pi@{self.ui.ip_1.value()}.{self.ui.ip_2.value()}.{self.ui.ip_3.value()}.{self.ui.ip_4.value()}" f" {path_venv_python} {path_check_cam}")
 
     def send_cmd(self) -> None:
         results = sp.run(
@@ -172,28 +175,28 @@ class TheMainWindow(QMainWindow):
             text=True,
         )
 
-        self.ui.text_output.setText(
-            results.stdout + "--------------------------\n" + results.stderr
-        )
+        self.ui.text_output.setText(results.stdout + "--------------------------\n" + results.stderr)
         self.subprocess_status(results)
 
-    def download_img_and_show(self, index:int) -> None:
+    def download_img_and_show(self, index: int) -> None:
         results = sp.run(
-            ["scp", f"pi@{self.ui.ip_1.value()}.{self.ui.ip_2.value()}.{self.ui.ip_3.value()}.{self.ui.ip_4.value()}:/tmp/cam{index}.npy", f"/tmp/cam{index}.npy"],
+            [
+                "scp",
+                f"pi@{self.ui.ip_1.value()}.{self.ui.ip_2.value()}.{self.ui.ip_3.value()}.{self.ui.ip_4.value()}:/tmp/cam{index}.npy",
+                f"/tmp/cam{index}.npy",
+            ],
             capture_output=True,
             text=True,
         )
         self.subprocess_status(results)
 
-        self.ui.text_output.setText(
-            results.stdout + "--------------------------\n" + results.stderr
-        )
-        img = np.load(f"/tmp/cam{index}.npy")[:, :, 0].T#.reshape(480, 640, 1, 3)
+        self.ui.text_output.setText(results.stdout + "--------------------------\n" + results.stderr)
+        img = np.load(f"/tmp/cam{index}.npy")[:, :, 0].T  # .reshape(480, 640, 1, 3)
 
         self.ui.image_view.setImage(
             img=img,
-            #levels=img.max(),
-            #axes={"x":1, "y":0, "c":2}
+            # levels=img.max(),
+            # axes={"x":1, "y":0, "c":2}
         )
 
     def subprocess_status(self, results: sp.CompletedProcess[str]):
@@ -207,5 +210,42 @@ class TheMainWindow(QMainWindow):
 
         button = dlg.exec_()
 
-        if button == QMessageBox.Ok:
+        if button == QMessageBox.StandardButton.Ok:
             print("OK!")
+
+    def when_sky_preview_button_clicked(self) -> None:
+        # shot at the this exposure
+        self.ui.le_cmd2send.setText(
+            f"ssh pi@{self.ui.ip_sky_1.value()}.{self.ui.ip_sky_2.value()}.{self.ui.ip_sky_3.value()}.{self.ui.ip_sky_4.value()}"
+            " python GndLogger/main2.py"
+            "  op_mode=1shot"
+            f" time={self.ui.sp_sky_duration.value()}"
+            f" expo_index={self.ui.d_sky_expo.value()}"
+            f" ENTER"
+        )
+
+        self.send_cmd()
+        # download the image
+        results = sp.run(
+            [
+                "scp",
+                f"pi@{self.ui.ip_sky_1.value()}.{self.ui.ip_sky_2.value()}.{self.ui.ip_sky_3.value()}.{self.ui.ip_sky_4.value()}:/tmp/1shot_preview.npy",
+                "/tmp/sky_preview.npy",
+            ],
+            capture_output=True,
+            text=True,
+        )
+        self.subprocess_status(results)
+
+        self.ui.text_output.setText(results.stdout + "--------------------------\n" + results.stderr)
+        img = np.load("/tmp/sky_preview.npy")
+
+        self.ui.image_view.setImage(
+            img=img,
+            # levels=img.max(),
+            # axes={"x":1, "y":0, "c":2}
+        )
+
+        self.ui.tabWidget_2.setCurrentIndex(1)
+
+        pass

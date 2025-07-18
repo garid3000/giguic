50550a073fca0494ad1c458d3d11bc1db32d81e2
garid_on_x13 <garidzorigoo@gmail.com>
2025-07-19 00:13:48 +0900

    update

--- default.nix
+++ default.nix
@@ -11,25 +11,47 @@
 #  ];
 #}
 
-let
-  pkgs = import <nixpkgs> {};
-in pkgs.mkShell {
+# let
+#   pkgs = import <nixpkgs> {};
+# in pkgs.mkShell {
+#   packages = [
+#     (pkgs.python3.withPackages (python-pkgs: [
+#       python-pkgs.pyside6
+#       python-pkgs.pyside2
+#       python-pkgs.numpy
+#       python-pkgs.sympy
+#       python-pkgs.opencv4
+#       python-pkgs.pyzmq
+#       python-pkgs.pyqtgraph
+#       python-pkgs.pyqt6
+#       pkgs.libsForQt5.qt5.wrapQtAppsHook
+#     ])
+#     )
+#   ];
+# }
+
+# save this as shell.nix
+{
+  pkgs ? import <nixpkgs> { },
+}:
+
+pkgs.mkShell {
   packages = [
     (pkgs.python3.withPackages (python-pkgs: [
       python-pkgs.pyside6
-      python-pkgs.pyside2
+      #python-pkgs.pyside2
       python-pkgs.numpy
-      python-pkgs.sympy
+      #python-pkgs.sympy
       python-pkgs.opencv4
-      python-pkgs.pyzmq
+      #python-pkgs.pyzmq
       python-pkgs.pyqtgraph
-      python-pkgs.pyqt6
-      pkgs.libsForQt5.qt5.wrapQtAppsHook
+      #python-pkgs.pyqt6
+      #pkgs.libsForQt5.qt5.wrapQtAppsHook
+      pkgs.pyright
     ])
     )
   ];
 }
-
 
 # also need to install nixgl, https://github.com/guibou/nixGL
 # and nixGL python3 main.py
